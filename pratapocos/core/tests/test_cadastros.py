ac5940006341d53efb75a255cc10b7ecae22d99b
williamfoschiera <william.foschiera@buser.com.br>
2023-05-08 09:32:54 -0400

    simplify tests

--- pratapocos/core/tests/test_cadastros.py
+++ pratapocos/core/tests/test_cadastros.py
@@ -1,5 +1,3 @@
-from pratapocos.accounts.models import User
-from pratapocos.accounts.tests import fixtures
 from pratapocos.core.models import Cadastro
 
 
@@ -8,20 +6,16 @@ def test_criar_cadastro_sem_login(client):
     assert resp.status_code == 401
 
 
-def test_criar_cadastro_com_login(client, db):
-    fixtures.user_jon()
-    client.force_login(User.objects.get(username="jon"))
-    resp = client.post("/api/core/cadastros/add", {"new_cadastro": "walk the dog"})
+def test_criar_cadastro_com_login(client_with_logged_user):
+    resp = client_with_logged_user.post("/api/core/cadastros/add", {"description": "uma nova tarefa"})
     assert resp.status_code == 200
 
 
-def test_criar_cadastro_com_login_2(client, db):
-    fixtures.user_jon()
-    Cadastro.objects.create(description="walk the dog")
+def test_listar_cadastro_com_login(client_with_logged_user):
+    new_cadastro = Cadastro.objects.create(description="walk the dog")
 
-    client.force_login(User.objects.get(username="jon"))
-    resp = client.get("/api/core/cadastros/list")
+    resp = client_with_logged_user.get("/api/core/cadastros/list")
     data = resp.json()
 
     assert resp.status_code == 200
-    assert data == {"cadastros": [{"description": "walk the dog", "done": False, "id": 1}]}
+    assert data == {"cadastros": [{"description": "walk the dog", "done": False, "id": new_cadastro.id}]}
